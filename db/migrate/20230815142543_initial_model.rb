7c583cf667557fe50be4d2a11869024633f2057b
sirramongabriel <sirramongabriel@gmail.com>
2023-08-21 15:59:44 -0400

    Commit initial model and seedfile

--- db/migrate/20230815142543_initial_model.rb
+++ db/migrate/20230815142543_initial_model.rb
@@ -1,34 +1,28 @@
 class InitialModel < ActiveRecord::Migration[7.0]
   def change
     create_table :addresses do |t|
-      t.string :street_address, null: false
-      t.string :city, null: false
-      t.float  :lat
-      t.float  :long
-      t.string :state, null: false
-      t.string :zip_code, null: false
-      t.string :addressable_id, null: false
-      t.string :addressable_type, null: false
+      t.integer :addressable_id 
+      t.string :addressable_type
+      t.string :street_address
+      t.string :city
+      t.string :state
+      t.string :zip_code
 
       t.timestamps
     end
-    add_index "addresses", [:addressable_id, :addressable_type], name: "index_addresses_by_id_and_type"
 
     create_table :drivers do |t|
-
       t.timestamps
     end
 
     create_table :rides do |t|
-      t.integer :score
-      t.integer :earnings
-      t.string :duration_in_hours
+      t.integer :driver_id
       t.string :distance_in_miles
-      t.string :commute_duration_in_hours
-      t.string :commute_distance_in_miles
-      t.string :driving_duration_in_hours
-      t.string :driving_distance_in_miles
-
+      t.string :duration_in_minutes 
+      t.string :commute_distance_in_miles 
+      t.string :commute_duration_in_minutes
+      t.integer :earnings 
+      t.float :score
       t.timestamps
     end
     add_index "rides", [:score], name: "index_rides_by_score"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,77 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Driver.delete_all
+puts "...deleteing old data"
+
+addresses = JSON.parse(File.read(Rails.root + "db/random_us_addresses.json"))
+ride_start_addresses = addresses.slice(0..121) # split total addresses 
+ride_destination_addresses = addresses.slice(122..245)
+
+driver = Driver.create!()
+driver.build_home_address(
+  addressable_id: driver.id,
+  street_address: "151 Main Street",
+  city: "Savannah",
+  state: "GA",
+  zip_code: "31408"
+)
+
+rides = []
+122.times do
+  ride = driver.rides.create!(
+    driver_id: driver.id,
+  )
+  rides << ride
+end
+
+ride_start_addresses.each_with_index do |start_address, index|
+  start_address = rides[index].build_start_address(
+    addressable_id: rides[index].id,
+    street_address: start_address["address1"],
+    city: start_address["city"],
+    state: start_address["state"],
+    zip_code: start_address["postalCode"]
+  )
+  puts "start address: => #{rides[index].start_address.to_json}"
+end
+
+ride_destination_addresses.each_with_index do |destination_address, index|
+  destination_address = rides[index].build_destination_address(
+    addressable_id: rides[index].id,
+    street_address: destination_address["address1"],
+    city: destination_address["city"],
+    state: destination_address["state"],
+    zip_code: destination_address["postalCode"]
+  )
+  puts "destination address: => #{rides[index].destination_address.to_json}"
+end
+
+rides.each do |ride|
+  puts "calculating ride data..."
+  ride_start_address = "#{ride.start_address.street_address.to_json} #{ride.start_address.city.to_json} #{ride.start_address.state.to_json} #{ride.start_address.zip_code.to_json}"
+  ride_destination_address = "#{ride.destination_address.street_address.to_json} #{ride.destination_address.city.to_json} #{ride.destination_address.state.to_json} #{ride.destination_address.zip_code.to_json}"
+  commute_start_address = "#{driver.home_address.street_address.to_json} #{driver.home_address.city.to_json} #{driver.home_address.state.to_json} #{driver.home_address.zip_code.to_json}"
+  
+  ride_response = Ride.calculate_distance_and_duration(ride_start_address, ride_destination_address)
+  commute_response = Ride.calculate_distance_and_duration(commute_start_address, ride_start_address)
+  earnings_response = Ride.calculate_earnings(ride_response[:distance_in_miles], ride_response[:distance_in_minutes])
+  score_response = Ride.calculate_score(commute_response[:duration_in_minutes], ride_response[:duration_in_minutes], ride_response[:distance_in_miles])
+
+  ride.update(
+    distance_in_miles: ride_response[:distance_in_miles], 
+    duration_in_minutes: ride_response[:duration_in_minutes], 
+    commute_distance_in_miles: commute_response[:distance_in_miles], 
+    commute_duration_in_minutes: commute_response[:duration_in_minutes], 
+    earnings: earnings_response,score: score_response
+  )
+end
+puts "<---------------Complete!---------------->"
+
+
+
+
+
+  
+
