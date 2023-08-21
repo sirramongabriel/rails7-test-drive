3e87d85802ad314791b47c539c7507edb1a39feb
sirramongabriel <sirramongabriel@gmail.com>
2023-08-21 15:54:37 -0400

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,14 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_08_15_142543) do
   create_table "addresses", force: :cascade do |t|
-    t.string "street_address", null: false
-    t.string "city", null: false
-    t.float "lat"
-    t.float "long"
-    t.string "state", null: false
-    t.string "zip_code", null: false
-    t.string "addressable_id", null: false
-    t.string "addressable_type", null: false
+    t.integer "addressable_id"
+    t.string "addressable_type"
+    t.string "street_address"
+    t.string "city"
+    t.string "state"
+    t.string "zip_code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["addressable_id", "addressable_type"], name: "index_addresses_by_id_and_type"
   end
 
   create_table "drivers", force: :cascade do |t|
@@ -31,14 +28,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_15_142543) do
   end
 
   create_table "rides", force: :cascade do |t|
-    t.integer "score"
-    t.integer "earnings"
-    t.string "duration_in_hours"
+    t.integer "driver_id"
     t.string "distance_in_miles"
-    t.string "commute_duration_in_hours"
+    t.string "duration_in_minutes"
     t.string "commute_distance_in_miles"
-    t.string "driving_duration_in_hours"
-    t.string "driving_distance_in_miles"
+    t.string "commute_duration_in_minutes"
+    t.integer "earnings"
+    t.float "score"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["score"], name: "index_rides_by_score"
