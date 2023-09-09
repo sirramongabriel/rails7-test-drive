406b5563dbd052dcfc860763155971a56c155094
sirramongabriel <sirramongabriel@gmail.com>
2023-09-09 16:39:49 -0400

    Enhance test coverage add missing class name for association

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -19,7 +19,7 @@
 #
 class Ride < ApplicationRecord
   has_one :start_address, as: :addressable, class_name: 'Address',  dependent: :destroy 
-  has_one :destination_address, as: :addressable, dependent: :destroy
+  has_one :destination_address, as: :addressable, class_name: 'Address', dependent: :destroy
   belongs_to :driver, optional: true
 
   @headers = {
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -33,4 +33,41 @@ RSpec.describe Ride, type: :model do
     it { should have_db_column(:earnings).of_type(:integer) }
     it { should have_db_column(:score).of_type(:float) }
   end
+
+  describe '.calculate_earnings' do
+    # calulate_earnings formula: $12 + $1.50 per mile beyond 5 miles + (ride duration) * $0.70 per minute beyond 15 minutes
+
+    context 'within the flatrate parameters'
+    let(:distance) { 5 }
+    let(:duration) { 15 }
+
+    it 'calculates the earnings correctly' do
+      earnings = Ride.calculate_earnings(distance, duration)
+      expect(earnings).to eq('12.00')
+    end
+
+    context 'correct calculation with a longer distance' do
+      let(:distance) { 8 }
+
+      it { expect(Ride.calculate_earnings(distance, duration)).to eq('16.50') }
+    end
+
+    context 'correct calculation with a longer duration' do
+      let(:duration) { 20 }
+
+      it { expect(Ride.calculate_earnings(distance, duration)).to eq('15.50') }
+    end
+  end
+
+  describe '.calculate_score' do
+    let(:commute_duration) { 30 }
+    let(:ride_duration) { 45 } 
+    let(:ride_distance) { 5 }
+
+    it 'calculates the score correctly' do
+      score = Ride.calculate_score(commute_duration, ride_duration, ride_distance)
+
+      expect(score).to eq(0.44) 
+    end
+  end
 end
